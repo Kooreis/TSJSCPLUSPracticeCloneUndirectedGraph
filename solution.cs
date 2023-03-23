bdd55cabfe397cbdaaac736eb19d50a9229befd0
Kooreis <kooreis.official@gmail.com>
2023-03-23 10:37:02 -0500

    Created a function to clone a graph. It uses a map to keep track of nodes that have been cloned. If a node has not been cloned, it creates a new node with the same value and adds it to the map. It then sets the neighbors of the new node to be the cloned versions of the original node's neighbors.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,14 @@
-class Node {
-    constructor(val, neighbors) {
-        this.val = val === undefined ? 0 : val;
-        this.neighbors = neighbors === undefined ? [] : neighbors;
+function cloneGraph(node) {
+    if (!node) return node;
+    const map = new Map();
+    return clone(node);
+
+    function clone(node) {
+        if (!map.has(node)) {
+            const newNode = new Node(node.val);
+            map.set(node, newNode);
+            newNode.neighbors = node.neighbors.map(clone);
+        }
+        return map.get(node);
     }
 }
